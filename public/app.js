cc11d215865c50bd41012f3d738fe910b948fb80
Marcos <mrgozu@hotmail.com>
2021-03-22 22:02:38 -0300

    Actualizacion front

--- public/app.js
+++ public/app.js
@@ -18,19 +18,17 @@ const crearHtml = () =>{
     const html = `   
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
     <div class="container">
-        <a class="navbar-brand mx-5" href="#">BsaleTest</a>
+        <a class="navbar-brand mx-5" onclick=recargarLista() href="#">BsaleTest</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
-            <form class="form-inline my-2 my-lg-0 w-100" id='buscador' >
-                <input class="form-control mr-sm-2 w-75" type="search" placeholder="Search" aria-label="Search" id='inputBusqueda' name='inputBusqueda'/>
-                <button class="btn " type="submit">
+                <input class="form-control mr-sm-2 w-75" type="search" placeholder="" onkeyup="eventoBusqueda(event)" aria-label="Search" id='inputBuscador' name='inputBuscador'/>
+                <button class="btn" onclick="eventoBusqueda()" type="button">
                     <i class="fas fa-search"></i>
                 </button>
-            </form>
         </div>
         <button class="btn " type="button" id='carro'>
         <i class="fas fa-shopping-cart"></i>
@@ -38,7 +36,7 @@ const crearHtml = () =>{
     </div>
 </nav>
 <div class="container">
-    <div class="row row-cols-1 row-cols-md-4" id="cardProducto">
+    <div class="row row-cols-1 row-cols-md-3" id="cardProducto">
             
       </div>
 
@@ -51,7 +49,7 @@ const crearHtml = () =>{
 // eventos (obtener id, btn, etc)
 const eventos = ()=>{
     card  = document.getElementById('cardProducto');
-    buscador = document.querySelector('#buscador');
+    // buscador = document.querySelector('#buscador');
     inputBuscador=document.getElementById('inputBuscador');
     carrito = document.getElementById('carro');
     
@@ -60,12 +58,14 @@ const eventos = ()=>{
 //  Card en que se mostraran los productos, recibe listado de productos
 const mostrarProducto =    (producto)=>{
         const divCard = document.createElement('div');
+        //Se aproxima haciaa el numero superior del precio descuento (para evitar decimales)
+        precioMostrado = Math.ceil(producto.discountPrice); 
         divCard.innerHTML = `
-        <div class='card'>
+        <div class='card h-100'>
         <img src="${producto.url_image||'./assets/img/image-not-found.png'}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${producto.name}</h5>
-            <p class="card-text">${producto.discountPrice}</p>
+            <p class="card-text">$${precioMostrado}</p>
         </div>
         </div>`;
         divCard.classList.add('col')
@@ -74,14 +74,39 @@ const mostrarProducto =    (producto)=>{
 }
 // Muestra todos los productos
 const obtenerListaProductos = async()=>{
+   
     const productos = await obtenerDatos();
     productos.forEach(mostrarProducto);
 }
 //Busqueda por nombre
 const busqueda = async(termino)=>{
-    const productos = await obtenerDatos(`busqueda/${termino}`);
+    const resultadoBusqueda = await obtenerDatos(`busqueda/${termino}`);
+    resultadoBusqueda.forEach(mostrarProducto);
+}
+//RecargarProductos
+const recargarLista =async ()=>{
+    while (card.firstChild) {
+        card.removeChild(card.firstChild);
+      }
+    const productos = await obtenerDatos();
     productos.forEach(mostrarProducto);
 }
+
+
+//eventoBusqueda
+const eventoBusqueda = (event)=>{
+    if(event &&event.keyCode!='13'){
+    }else{
+        while (card.firstChild) {
+            card.removeChild(card.firstChild);
+          }
+        busqueda(inputBuscador.value);
+    }
+    
+    
+    
+}
+
 
 
 //  construccion html
